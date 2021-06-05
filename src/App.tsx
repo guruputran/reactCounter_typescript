c4fb181e1c186b36d273fcec39f124af4811d24f
guruputran <gpsmartcodes@gmail.com>
2021-06-05 08:13:35 +0800

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/** @format */
 
-function App() {
+import CountStore from "./CountStore";
+import CountList from "./components/CountList";
+
+const App = (): JSX.Element => {
+  const store = new CountStore();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CountList store={store} />
     </div>
   );
 }
